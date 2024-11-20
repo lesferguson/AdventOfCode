56a525c66b00eea9be802d34ba1484c9cbbcde2f
Les Ferguson <les.ferguson.42@gmail.com>
2024-11-20 17:58:49 -0600

    Testing using the advent of code data library, and starting with previous years puzzles.

---
+++ template.py
@@ -0,0 +1,52 @@
+from aocd import data, submit
+from aocd.models import Puzzle
+import os
+
+
+def solve_a(input_data):
+    solution = 0
+
+
+
+
+    return solution
+
+
+def solve_b(input_data):
+    solution = 0
+
+
+
+
+    return solution
+
+
+
+
+
+year, day = [ int(param.strip(".py")) for param in os.path.abspath(__file__).split('\\')[-2:]]
+puzzle = Puzzle(year=year, day=day)
+
+for example in puzzle._get_examples():
+    if not puzzle.answered_a and example.answer_a:
+        print("-----------------")
+        print("input: ", example.input_data)
+        print("example a answer: ", example.answer_a)
+        sol = solve_a(example.input_data)
+        print("solution a answer: ", sol)
+        print(sol)
+        assert str(sol) == example.answer_a
+        print("Example Passed")
+    elif example.answer_b:
+        print("-----------------")
+        print("input: ", example.input_data)
+        print("example b answer: ", example.answer_b)
+        sol = solve_b(example.input_data)
+        print("solution b answer: ", sol)
+        # print(example.extra)
+        assert str(sol) == example.answer_b
+        print("Example Passed")
+if not example.answer_b:
+    submit(solve_a(data))
+else:
+    submit(solve_b(data))
